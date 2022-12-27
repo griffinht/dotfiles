5b86d32575a7428f0802fed4cc15f3f4b16051af
griffinht <git@griffinht.com>
2022-12-27 10:32:22 -0500

    start to add arkenfox firefox

---
+++ home/firefox/user-overrides.js
@@ -0,0 +1,13 @@
+user_pref("privacy.resistFingerprinting.letterboxing", false);
+user_pref("extensions.pocket.enabled", false);
+user_pref("keyword.enabled", true);
+
+//user_pref("privacy.clearOnShutdown.cache", true);     // [DEFAULT: true]
+user_pref("privacy.clearOnShutdown.history", false);   // [DEFAULT: true]
+//user_pref("privacy.clearOnShutdown.sessions", true);  // [DEFAULT: true]
+
+//user_pref("privacy.cpd.cache", true);    // [DEFAULT: true]
+user_pref("privacy.cpd.history", false);  // [DEFAULT: true]
+//user_pref("privacy.cpd.sessions", true); // [DEFAULT: true]
+
+user_pref("signon.rememberSignons", false);  // [DEFAULT: true]
