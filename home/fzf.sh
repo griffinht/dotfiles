dca45aa84591c400823eef164f2366b9a29565ba
griffinht <git@griffinht.com>
2023-04-05 18:37:51 -0400

    start to add fzf script

--- home/fzf.sh
+++ home/fzf.sh
@@ -1,8 +1,22 @@
 #!/usr/bin/env sh
 
-export RG_PREFIX="rg --color=always --smart-case --line-number"
-FZF_DEFAULT_COMMAND="$RG_PREFIX ''" \
-    fzf \
-    --disabled --ansi \
-    --delimiter ':' --preview 'bat --color=always {1} --highlight-line {2}' \
-    --bind 'change:reload:$RG_PREFIX {q} || true'
+#cmd fzf_search ${{
+# column not used?
+result="$( \
+    RG_PREFIX="rg --color=always --smart-case --column --line-number --heading --context=5"
+    FZF_DEFAULT_COMMAND="$RG_PREFIX ''" \
+        fzf \
+        --disabled --ansi \
+        --bind "change:reload:$RG_PREFIX {q} || true" \
+        | cut -d ':' -f 1
+)"
+
+#--layout-reverse?
+#--header 'header'?
+    #--delimiter ':' --preview 'bat --color=always {1} --highlight-line={2}' \
+if [ ! -z "$result" ]; then
+    lf -remote "send $id select \"$result\""
+fi
+#}}
+
+#map gs :fzf_search
