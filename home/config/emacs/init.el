6fd2c09dce90d61efb490c031edf373bcbd99c29
griffinht <git@griffinht.com>
2023-05-29 15:51:17 -0400

    emacs add todo

--- home/config/emacs/init.el
+++ home/config/emacs/init.el
@@ -1,3 +1,12 @@
+
+;https://www.labri.fr/perso/nrougier/GTD/index.html
+;https://sqrtminusone.xyz/posts/2023-04-13-emacs/
+;https://libreddit.hot.griffinht.com/r/GUIX/comments/pv454k/how_do_you_manage_your_emacs_config_within_the/
+
+;https://github.com/Fanael/rainbow-identifiers
+;https://github.com/ankurdave/color-identifiers-mode
+;https://www.masteringemacs.org/article/how-to-get-started-tree-sitter
+
 ;; Enable Evil
 (require 'evil)
 (evil-mode 1)
