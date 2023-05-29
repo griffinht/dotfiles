513fc534d7be2c1097249ca622a1a2d21ebd5f67
griffinht <git@griffinht.com>
2023-05-28 22:36:30 -0400

    add basic emacs config, syntax highlighting is disapointing

---
+++ home/config/emacs/init.el
@@ -0,0 +1,7 @@
+;; Enable Evil
+(require 'evil)
+(evil-mode 1)
+(load-theme 'solarized-dark t)
+;; https://codeberg.org/dnkl/foot/wiki#user-content-only-8-colors-in-emacs
+;; https://codeberg.org/dnkl/foot/wiki#user-content-24-bit-color-support-in-emacs
+;;(add-to-list 'term-file-aliases '("foot" . "xterm"))
