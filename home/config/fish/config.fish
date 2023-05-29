abbace2db8725181e6ec35175fa685281c4cbf95
griffinht <git@griffinht.com>
2023-05-28 20:17:56 -0400

    fish vim mode

--- home/config/fish/config.fish
+++ home/config/fish/config.fish
@@ -22,3 +22,19 @@ end
 function fish_prompt
     printf '%s$%s ' (set_color $fish_color_cwd) (set_color normal)
 end
+
+# https://fishshell.com/docs/current/interactive.html#vi-mode-commands
+function fish_user_key_bindings
+    # Execute this once per mode that emacs bindings should be used in
+    fish_default_key_bindings -M insert
+
+    # Then execute the vi-bindings so they take precedence when there's a conflict.
+    # Without --no-erase fish_vi_key_bindings will default to
+    # resetting all bindings.
+    # The argument specifies the initial mode (insert, "default" or visual).
+    fish_vi_key_bindings --no-erase insert
+end
+
+# disable ugly mode prompt
+function fish_mode_prompt
+end
