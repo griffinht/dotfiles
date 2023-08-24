0307a4df54b87d56ff37afe20b53432c15cf8916
griffinht <git@griffinht.com>
2023-08-24 14:12:54 -0400

    add eclipse launch scripts

---
+++ home/bin/eclipse.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+#https://libreddit.hot.griffinht.com/r/GUIX/comments/11tljcb/running_eclipse_ide_in_gnu_guix_system/
+
+guix shell \
+    --container \
+    --emulate-fhs \
+    --network \
+    --preserve='^DBUS_' --expose=/var/run/dbus \
+    --preserve='^DISPLAY$' \
+    --expose=/sys/dev --expose=/sys/devices --expose=/dev/dri \
+    --share=$HOME \
+    which coreutils glib gtk+ glibc libxtst adwaita-icon-theme openjdk@17:jdk \
+    -- $@
