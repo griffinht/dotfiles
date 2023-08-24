1d763da138bb614a72366647074c8c5345869ea6
griffinht <git@griffinht.com>
2023-08-24 09:18:38 -0400

    configure firefox

--- home/firefox/user.js
+++ home/firefox/user.js
@@ -1,5 +1,3 @@
 user_pref("extensions.pocket.enabled", false);
-// disable search suggestions
-user_pref("keyword.enabled", false);
 // disable built in password manager, i use my own
 user_pref("signon.rememberSignons", false);
