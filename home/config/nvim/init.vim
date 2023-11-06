8bad1d46c1a868e2deb8c39abccb54d09d77c9e9
griffinht <git@griffinht.com>
2023-11-06 14:21:06 -0500

    refactor init.vim rtp

--- home/config/nvim/init.vim
+++ home/config/nvim/init.vim
@@ -1,3 +1,5 @@
+lua require("init")
+
 " line numbers
 set number
 " make them relative numbers
@@ -19,11 +21,6 @@ set laststatus=0
 set noshowmode
 
 
-" todo package this
-" temporary auto save installation plugin
-" toggle with :ASToggle
-set rtp+=~/git/auto-save.nvim
-
 " todo set titlestring=
 "https://neovim.discourse.group/t/help-needed-customizing-nvim-terminal-title/721
 
@@ -32,17 +29,9 @@ set rtp+=~/git/auto-save.nvim
 "https://github.com/nvim-treesitter/nvim-treesitter#supported-languages
 "https://hot.griffinht.com/searx/search?t=ffab&q=neovim+solarized+
 
-" terrible hack to add guix vim package to neovim
-" this will probably break things frequently
-set rtp+=~/.guix-home/profile/share/vim/vimfiles
 " disable solarized double background
 let g:solarized_termtrans=1
 colorscheme solarized
-
-" hledger vim
-set rtp+=~/git/vim-ledger
-
-lua require("init")
 
 " spellchecking
 " disable with :nospell
--- home/config/nvim/lua/init.lua
+++ home/config/nvim/lua/init.lua
@@ -1,3 +1,146 @@
+-- IMPORTS
+-- these are all downloaded with the nvim.mk makefile
+
+-- toggle with :ASToggle
+vim.opt.runtimepath:append(",~/git/auto-save.nvim")
+
+-- hledger vim
+vim.opt.runtimepath:append(",~/git/vim-ledger")
+
+-- terrible hack to add guix vim package to neovim
+-- this will probably break things frequently
+vim.opt.runtimepath:append(",~/.guix-home/profile/share/vim/vimfiles")
+
+-- language server stuff
+vim.opt.runtimepath:append(",~/git/nvim-lspconfig")
+vim.opt.runtimepath:append(",~/git/nvim-jdtls")
+
+
+
+
+
+
+-- Set up lspconfig.
+-- https://github.com/hrsh7th/cmp-nvim-lsp
+--local capabilities = require('cmp_nvim_lsp').default_capabilities()
+-- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+
+local lspconfig = require("lspconfig")
+--local lsp_defaults = lspconfig.util.default_config
+
+--[[
+lsp_defaults.capabilities = vim.tbl_deep_extend(
+  'force',
+  lsp_defaults.capabilities,
+  require('cmp_nvim_lsp').default_capabilities()
+)]]--
+
+--todo https://github.com/mfussenegger/nvim-jdtls
+--lspconfig.jdtls.setup ({})
+--use nvim-jdtls instead of lspconfig's jdtls (has more features) -- https://github.com/mfussenegger/nvim-jdtls
+
+lspconfig.clangd.setup ({})
+lspconfig.pylsp.setup {}
+lspconfig.gopls.setup {}
+lspconfig.lua_ls.setup {
+    on_init = function(client)
+        client.config.settings = vim.tbl_deep_extend("force", client.config.settings, {
+            Lua = {
+                runtime = {
+                    -- Tell the language server which version of Lua you're using
+                    -- (most likely LuaJIT in the case of Neovim)
+                    version = "LuaJIT"
+                },
+                -- Make the server aware of Neovim runtime files
+                workspace = {
+                    checkThirdParty = false,
+                    --[[
+                    library = {
+                        vim.env.VIMRUNTIME
+                        -- "${3rd}/luv/library"
+                        -- "${3rd}/busted/library",
+                    }]]--
+                    -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+                     library = vim.api.nvim_get_runtime_file("", true)
+                }
+            }
+        })
+
+        --client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+        return true
+    end,
+}
+
+
+-- jump around C-], C-O back C-I forward
+-- :noh clear search
+-- todo use space leader key?
+-- <space>f for example
+--vim.keymap.del("n", "k")
+vim.keymap.set("n", "<Tab>", function()
+    local buffer = vim.diagnostic.open_float()
+    print(buffer)
+    vim.keymap.set("n", "<Enter>", ":q<CR>", {buffer = buffer})
+    vim.keymap.set("n", "<Esc>", ":q<CR>", {buffer = buffer})
+end)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
+
+
+vim.keymap.set("i", "<C-Space>", "<C-X><C-O>")
+
+--pretty great example of auto closeable and stuff
+--https://neovim.discourse.group/t/how-to-show-diagnostics-on-hover/3830/3
+
+-- TODO TODO TODO
+--https://github.com/neovim/nvim-lspconfig/wiki/Autocompletion
+
+-- great defaults https://github.com/neovim/nvim-lspconfig#suggested-configuration
+vim.api.nvim_create_autocmd("LspAttach", {
+    -- i think this prevents the group from being created multiple times maybe
+    -- idk why its necessary
+    -- todo see :help nvim_create_augroup
+    group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+    callback = function(event)
+        -- lsp buffer
+
+        -- Enable completion triggered by <c-x><c-o>
+        vim.bo[event.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+
+        -- local mappings
+        local options = { buffer = event.buf }
+
+        -- hover
+        --todo this is set by man.lua??? huh?
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, options)
+        vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, options)
+        vim.keymap.set("n", "<C-S-K>", vim.lsp.buf.document_symbol)
+
+        -- go to
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, options)
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, options)
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, options)
+        vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, options)
+        vim.keymap.set("n", "gr", vim.lsp.buf.references)
+
+        -- actions
+        vim.keymap.set("n", "<C-Tab>", vim.lsp.buf.code_action)
+        vim.keymap.set("n", "<S-R>", vim.lsp.buf.rename)
+        --vim.keymap.set("i", "<S-Space>", vim.lsp.buf.completion)
+        --todo buf format?
+        -- todo clangd implementation with compile_commands json
+        --https://neovim.discourse.group/t/clangd-lsp-jump-to-definition-takes-me-to-h-file/2153
+        --todo workspace folders
+    end,
+})
+
+
+
+
+
+
+
 --vim.opt.runtimepath:append(",~/git/cmp-nvim-lsp")
 --vim.opt.runtimepath:append(",~/git/nvim-lspconfig")
 
@@ -6,6 +149,7 @@
 --require("mytreesitter")
 --require("mylsp")
 
+--[[
 -- LUASNIP
 vim.opt.runtimepath:append(",~/git/luasnip")
 vim.opt.runtimepath:append(",~/git/friendly-snippets")
@@ -24,13 +168,15 @@ vim.keymap.set({"i", "s"}, "<C-E>", function()
 		ls.change_choice(1)
 	end
 end, {silent = true})
+]]--
 
 -- nvim-cmp
+--[[
 vim.opt.runtimepath:append(",~/git/nvim-cmp")
 vim.opt.runtimepath:append(",~/git/cmp_luasnip")
 vim.opt.runtimepath:append(",~/git/cmp-nvim-lsp")
 vim.opt.runtimepath:append(",~/git/cmp-buffer")
-vim.opt.runtimepath:append(",~/git/nvim-lspconfig")
+]]--
 
 --[[
 local cmp = require("cmp")
@@ -49,6 +195,7 @@ cmp.setup({
 
 ]]--
 
+--[[
 -- Set up nvim-cmp.
 local cmp = require('cmp')
 local luasnip = require('luasnip')
@@ -131,22 +278,6 @@ sources = cmp.config.sources({
   { name = 'cmdline' }
 })
 })
+]]--
 
--- Set up lspconfig.
--- https://github.com/hrsh7th/cmp-nvim-lsp
---local capabilities = require('cmp_nvim_lsp').default_capabilities()
--- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 
-local lspconfig = require('lspconfig')
-local lsp_defaults = lspconfig.util.default_config
-
-lsp_defaults.capabilities = vim.tbl_deep_extend(
-  'force',
-  lsp_defaults.capabilities,
-  require('cmp_nvim_lsp').default_capabilities()
-)
-
-lspconfig.jdtls.setup ({})
-lspconfig.clangd.setup ({})
-lspconfig.pylsp.setup {}
-lspconfig.gopls.setup {}
