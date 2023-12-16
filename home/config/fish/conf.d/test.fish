a148da0a94ca0b6fc85fc106c607ea522f597e98
griffinht <git@griffinht.com>
2023-12-16 08:41:25 -0500

    update fish config

--- home/config/fish/conf.d/test.fish
+++ home/config/fish/conf.d/test.fish
@@ -7,17 +7,19 @@ function copy_last_command
     history -n 1 | tee /dev/stderr | wl-copy
 end
 
+
 # bangarang
-function run_last_command
-    echo hi
-    #switch (commandline -t)
-    #    case "!"
-    #        commandline --current-token -- $history[1]
-    #        commandline --function repaint
-    #end
-    echo done
+function last_command_search_output
+    set --local search_engine_uri https://duckduckgo.com/?q=
+
+    xdg-open "$search_engine_uri($history[1])"
+    #commandline --current-token -- $history[1]
+    #commandline --function repaint
+    #J$history[1]
 end
 
-abbr gg run_last_command
+#bind -M default nn last_command_search_output
+
+#abbr gg run_last_command
 #bind -M insert ! run_last_command
 alias !!="echo hi"
