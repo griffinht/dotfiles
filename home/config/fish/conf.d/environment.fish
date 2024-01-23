ecaabe47492b0f4728c08c39473cd41a2201968a
griffinht <git@griffinht.com>
2024-01-23 16:09:05 -0500

    refix fish for real

--- home/config/fish/conf.d/environment.fish
+++ home/config/fish/conf.d/environment.fish
@@ -1,3 +1,10 @@
+if ! set -q FISH_IS_SETUP
+    return
+end
+
+set -x FISH_IS_SETUP
+
+
 #
 # ENVIRONMENT
 #
@@ -13,19 +20,16 @@ set -x SSL_CERT_FILE "$HOME/.guix-home/profile/etc/ssl/certs/ca-certificates.crt
 set -x GIT_SSL_CAINFO "$SSL_CERT_FILE"
 set -x GUIX_LOCPATH "$HOME/.guix-home/profile/lib/locale"
 
-if ! set -q FISHY
-    echo fishy
-    set -x FISHY
-    # add local bin for testing
-    set -x PATH "$HOME/.local/bin:$PATH"
-    # add missing guix-profile bin
-    # this is only because of scheme-lsp-server
-    # https://codeberg.org/rgherdt/scheme-lsp-server#installation-with-guix
-    # i wish i could install this to guix-home instead and have everything just work
-    set -x PATH "$HOME/.guix-profile/bin:$PATH"
-    set -x GUILE_LOAD_PATH "$HOME/.guix-profile/share/guile/site/3.0:$GUILE_LOAD_PATH"
-    set -x GUILE_LOAD_COMPILED_PATH "$HOME/.guix-profile/lib/guile/3.0/site-ccache:$GUILE_LOAD_COMPILED_PATH"
-end
+set -x FISHY
+# add local bin for testing
+set -x PATH "$HOME/.local/bin:$PATH"
+# add missing guix-profile bin
+# this is only because of scheme-lsp-server
+# https://codeberg.org/rgherdt/scheme-lsp-server#installation-with-guix
+# i wish i could install this to guix-home instead and have everything just work
+set -x PATH "$HOME/.guix-profile/bin:$PATH"
+set -x GUILE_LOAD_PATH "$HOME/.guix-profile/share/guile/site/3.0:$GUILE_LOAD_PATH"
+set -x GUILE_LOAD_COMPILED_PATH "$HOME/.guix-profile/lib/guile/3.0/site-ccache:$GUILE_LOAD_COMPILED_PATH"
 
 # use bat as pager
 # this is useless and the same as PAGER=less
@@ -46,9 +50,7 @@ set -x EDITOR "nvim"
 # pip install path
 set -x PIP_TARGET "$HOME/.local/share/pip"
 # pip install will put binaries here
-if ! set -q FISHY
-    # todo path priority?
-    set -x PATH "$PATH:$HOME/.local/share/pip/bin"
-    # this makes python script import work
-    set -x PYTHONPATH "$PYTHONPATH:$HOME/.local/share/pip"
-end
+# todo path priority?
+set -x PATH "$PATH:$HOME/.local/share/pip/bin"
+# this makes python script import work
+set -x PYTHONPATH "$PYTHONPATH:$HOME/.local/share/pip"
