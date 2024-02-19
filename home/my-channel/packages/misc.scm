d4968b11cc3f0a18c31e0714500e586f41c3c704
griffinht <git@griffinht.com>
2024-02-19 11:50:10 -0500

    my-channel: package drm_info

--- home/my-channel/packages/misc.scm
+++ home/my-channel/packages/misc.scm
@@ -2,8 +2,11 @@
                #:use-module (guix packages)
                #:use-module (guix download)
                #:use-module (guix git-download)
-               #:use-module (guix build-system gnu)
+               #:use-module (guix build-system meson)
                #:use-module (guix licenses)
+               #:use-module (gnu packages pkg-config)
+               #:use-module (gnu packages xdisorg)
+               #:use-module (gnu packages web)
                ;#:use-module (guix gexp)
                #:use-module (gnu packages autotools))
 
@@ -30,3 +33,23 @@
     (home-page "https://github.com/openSUSE/catatonit")
     (license gpl2)))
 |#
+(define-public
+  drm_info
+  (package
+    (name "drm_info")
+    (version "2.6.0")
+    (source
+      (origin
+        (method git-fetch)
+        (uri
+          (git-reference
+            (url "https://gitlab.freedesktop.org/emersion/drm_info")
+            (commit (string-append "v" version))))
+        (sha256 "0fc1rd3c16ddzbdpcj473ykszipzblj98lk376slk63v7mqvc1qm")))
+      (build-system meson-build-system)
+      (inputs
+        (list pkg-config libdrm json-c))
+      (synopsis "")
+      (description "")
+      (home-page "https://gitlab.freedesktop.org/emersion/drm_info")
+      (license expat)))
