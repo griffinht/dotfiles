81db69db7a7b48d4149e3cc3d71d184198267c29
griffinht <git@griffinht.com>
2024-05-11 15:26:20 -0400

    remove old script

--- home/bin/mount.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-set -e
-
-sudo "$(which wg-quick)" up hot_desktop
-sudo "$(which mount.nfs)" hot-desktop.wg.griffinht.com:/ /home/griffin/public
-sudo "$(which wg-quick)" up envy_laptop
